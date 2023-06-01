b95d2c57f2bfa3e5717b5ac1fc35a61e10e3de81
salishajose <salishajose@gmail.com>
2023-06-01 10:52:39 +0530

    personal web site version1

---
+++ main.js
@@ -0,0 +1,115 @@
+function validateForm() {
+  
+  const userNameValue = document.getElementById("name").value.trim();
+  const mobileNumberValue = document.getElementById("mobilenumber").value.trim();
+  const emailValue = document.getElementById("email").value.trim();
+  const subjectValue = document.getElementById("subject").value.trim();
+  const messageValue = document.getElementById("message").value.trim();
+  const msg1 = document.getElementById("msg1");
+
+  if (userNameValue === "") {
+    msg1.innerHTML = "Name must be filled out"
+    setTimeout(function () {
+      msg1.innerHTML = ""
+    }, 5000)
+    return false;
+  }
+
+  if (/\d/.test(userNameValue)) {
+    msg1.innerHTML = "Name must be character"
+    setTimeout(function () {
+      msg1.innerHTML = ""
+    }, 5000)
+    return false
+  }
+
+  if (mobileNumberValue === "") {
+    msg1.innerHTML = "Mobile Number must be filled out"
+    setTimeout(function () {
+      msg1.innerHTML = ""
+    }, 5000)
+    return false;
+  }
+
+  if (isNaN(mobileNumberValue)) {
+    msg1.innerHTML = "Mobile Number must be Digits"
+    setTimeout(function () {
+      msg1.innerHTML = ""
+    }, 5000)
+    return false
+  }
+
+  if (mobileNumberValue.length < 10) {
+    msg1.innerHTML = "Mobile Number must have 10 digits"
+    setTimeout(function () {
+      msg1.innerHTML = ""
+    }, 5000)
+    return false
+  }
+
+  if (mobileNumberValue.length > 10) {
+    msg1.innerHTML = "Mobile Number must have only 10 digits"
+    setTimeout(function () {
+      msg1.innerHTML = ""
+    }, 5000)
+    return false
+  }
+
+  if (emailValue === "") {
+    msg1.innerHTML = "Email must be filled out"
+    setTimeout(function () {
+      msg1.innerHTML = ""
+    }, 5000)
+    return false;
+  }
+
+  var emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (!emailRegex.test(emailValue)) {
+    msg1.innerHTML = "Email must be a valid email address"
+    setTimeout(function () {
+      msg1.innerHTML = ""
+    }, 5000)
+    return false;
+  }
+
+  if (subjectValue === "") {
+    msg1.innerHTML = "Subject must be filled out"
+    setTimeout(function () {
+      msg1.innerHTML = ""
+    }, 5000)
+    return false;
+  }
+
+
+  if (messageValue === "") {
+    msg1.innerHTML = "Message must be filled out"
+    setTimeout(function () {
+      msg1.innerHTML = ""
+    }, 5000)
+    return false;
+  }
+
+  return true;
+}
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyFsxcXOiS1Ikkoo6mGAWKvgRYvYWHyATVFRLcvUgjI09PnxsrH4H01wR4J9Ei1-s235w/exec'
+const form = document.getElementById("form")
+const msg = document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  if (validateForm()) {
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+      .then(response => {
+        alert("Form submitted successfully")
+        window.location.reload()
+        //msg.innerHTML = "Sent Successfully"
+        // alert("Sent Successfully")
+        setTimeout(function () {
+          msg.innerHTML = ""
+        }, 5000)
+        form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+  }
+})
